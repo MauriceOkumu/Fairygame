64bbd2efbce8a958bd619e39457a3fe5118bc3f1
mauriceokumu <maurice.okumu@thoughtworks.com>
2019-09-12 12:10:29 -0500

    Add bootstrap login form

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,36 +1,116 @@
-import React , { Component }from 'react';
 import Axios from 'axios';
+import React, { Component } from 'react';
+// import classnames from 'classnames';
+// import { bindActionCreators } from 'redux';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import { loginUser } from '../actions/authentication';
 
-class Login extends Component  {
-    constructor() {
-      super()
-      this.state = {
-          data: {}
-      }
+class Login extends Component {
+    constructor () {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {},
+            data: {}
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    componentDidMount () {
-     Axios.get('/api', {
-        headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-           }
-      })
-      .then(values => {
+
+    handleInputChange(e) {
         this.setState({
-            data:values.data
+            [e.target.name] : e.target.value
         })
-      })
-      // console.log('Hurray', this)
     }
+    
+    handleSubmit(e) {
+        e.preventDefault();
+        const { email, password} = this.state;
+        const user = {
+            email,
+            password
+        }
+        this.props.loginUser(user, this.props.history);
+    }
+    componentDidMount() {
+        // if(this.props.auth.isAuthenticated) {
+        //     this.props.history.push('/');
+        // }
+        Axios.get('/api', {
+                  headers : { 
+                      'Content-Type': 'application/json',
+                      'Accept': 'application/json'
+                     }
+                })
+                .then(values => {
+                  this.setState({
+                      data:values.data
+                  })
+                })
+                console.log('Hurray', this.state.data)
+    }
+    componentWillReceiveProps(nextProps) {
+        // if(nextProps.auth.isAuthenticated) {
+        //     this.props.history.push('/');
+        // }
+        // if(nextProps.errors) {
+        //     this.setState({
+        //         errors: nextProps.errors
+        //     })
+        // }
+    }
+
     render() {
-        const { data } = this.state
+        const { errors , data} = this.state;
+        console.log('Errors---->', errors)
         return (
-            <div className="Login">
-            <div>{data.Name}</div>
-              Hello
+            <div className="container">
+              <div>
+              <div>{data.Name}</div>
+//               Hello
+              </div>
+                <form onSubmit={ this.handleSubmit }>
+                    <div className="form-group">
+                        <input type="email" placeholder="email" 
+                        // className={classnames('form-control form-control-lg', {
+                        //     'is-invalid': errors.email
+                        //    })}
+                         onChange ={this.handleInputChange }
+                         value={ this.state.email } name="email" className="form-control"/>
+                         {/* {errors.email && (<div classnames="invalid-feedback">{errors.email}</div>)} */}
+                    </div>
+                    <div className="form-group">
+                        <input type="password" placeholder="password" 
+                        // className={classnames('form-control form-control-lg', {
+                            // 'is-invalid': errors.password
+                          //  })}
+                        onChange ={this.handleInputChange }
+                         value={ this.state.password} name="password" className="form-control"/>
+                         {/* {errors.password && (<div classnames="invalid-feedback">{errors.password}</div>)} */}
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">Login</button>
+                    </div>    
+                </form>
             </div>
-          );
+        )
     }
 }
 
-export default Login;
+// Login.propTypes = {
+//     errors: PropTypes.object.isRequired,
+//     auth: PropTypes.object.isRequired,
+//     errors: PropTypes.object.isRequired
+// }
+
+// const mapStateToProps = state => ({
+//     errors: state.errors,
+//     auth: state.auth
+// });
+
+// const mapDispatchToProps = dispatch => bindActionCreators({loginUser}, dispatch);
+
+export default Login
+// export default connect(mapStateToProps, mapDispatchToProps)(Login);
